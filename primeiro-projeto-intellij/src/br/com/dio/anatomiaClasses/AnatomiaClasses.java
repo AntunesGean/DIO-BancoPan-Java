b4a56c04fa1df9c7994c515f165726d1e03ee13c
AntunesGean <gean.antunes@gmail.com>
2023-03-06 16:11:21 -0300

    create AnatomiaClasses

---
+++ primeiro-projeto-intellij/src/br/com/dio/anatomiaClasses/AnatomiaClasses.java
@@ -0,0 +1,15 @@
+package br.com.dio.anatomiaClasses;
+
+public class AnatomiaClasses {
+    public static void main(String[] args) {
+
+        int ano = 2021;
+        ano = 2022; // pode ser reatribuido
+
+        final String BR = "Brasil";
+        // BR = "Brasil"; // Cannot assign a value to final variable 'BR'
+
+        System.out.println(ano);
+        System.out.println(BR);
+    }
+}
