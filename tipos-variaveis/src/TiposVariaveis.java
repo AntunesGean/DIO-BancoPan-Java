2ce291c539a8972b330b807b9b3ff44b9c70315a
AntunesGean <gean.antunes@gmail.com>
2023-03-21 15:45:58 -0300

    aula sobre tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salario = 2500.33;
+        final double PI = 3.14; // final = Constante
+        int idade = 25;
+        String nome = "João";
+        char sexo = 'M';
+        boolean casado = false;
+
+        System.out.println("Meu nome é " + nome);
+        System.out.println("Minha idade é " + idade);
+        System.out.println("Meu sexo é " + sexo);
+        System.out.println("Estou casado? " + casado);
+        System.out.println("Meu salário é " + salario);
+        System.out.println("O valor de PI é " + PI);
     }
 }
